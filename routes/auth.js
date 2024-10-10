896bfa871f4c25b0ab326fa31f4f6b5ee1268d05
Doğu Tozlu <dogutozlu2007@gmail.com>
2024-10-11 01:21:51 +0300

    Fixed password change

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const { User } = require('../models'); 
-const securityQuestions = require('../config/securityquestions');
+const securityQuestions = require('../config/securityQuestions');
 
 // GET Signup Page
 router.get('/signup', (req, res) => {
